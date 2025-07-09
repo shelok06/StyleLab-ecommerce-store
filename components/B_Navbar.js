9b0053869bedf0660a1f4b1405c7dc881d900e10
Shelok <sheloksamson@gmail.com>
2025-07-09 17:20:42 +0500

    UI changes

--- components/B_Navbar.js
+++ components/B_Navbar.js
@@ -15,40 +15,40 @@ const B_Navbar = () => {
 
   return (
     <>
-      <nav className='fixed bottom-0 w-full bg-[#065f42] block lg:hidden z-20 sm:px-5 p-2.5 sm:py-3'>
+      <nav className='fixed -bottom-1 w-full border-t-4 border-neutral-800 bg-[#f5f5f5b3] backdrop-blur-2xl block lg:hidden z-20 sm:px-5 p-2.5 sm:py-3'>
         <div className='flex items-center justify-evenly xs:justify-center'>
           <div className='flex justify-between items-center gap-4 xs:gap-8 sm:gap-15 mx-1 xs:mx-2.5'>
-            <Link href="/" className={pathname === "/" ? "bg-[#10b981] p-2.5 rounded-xl flex flex-col justify-center items-center text-white" : " flex flex-col justify-center items-center text-white"}>
+            <Link href="/" className={pathname === "/" ? "bg-neutral-200 p-2.5 rounded-xl flex flex-col justify-center items-center" : " flex flex-col justify-center items-center"}>
               <button>
-                <IoMdHome className='text-white size-6 sm:size-8' />
+                <IoMdHome className='size-6 sm:size-8' />
               </button>
               <div className='text-sm sm:text-base'>Home</div>
             </Link>
 
-            <Link href="/shop" className={pathname === "/shop" ? "bg-[#10b981] p-2.5 rounded-xl flex flex-col justify-center items-center text-white" : " flex flex-col justify-center items-center text-white"}>
+            <Link href="/shop" className={pathname === "/shop" ? "bg-neutral-200 p-2.5 rounded-xl flex flex-col justify-center items-center" : " flex flex-col justify-center items-center"}>
               <button>
-                <FaShoppingBag className='text-white size-6 sm:size-8' />
+                <FaShoppingBag className='size-6 sm:size-8' />
               </button>
               <div className='text-sm sm:text-base'>Shop</div>
             </Link>
           </div>
 
-          <Link href="/mycart"><div className="circle rounded-full border-8 border-[#065f42] bg-[#ecfdf7] p-3 mx-1 xs:mx-2.5 sm:mx-10">
+          <Link href="/mycart"><div className="circle rounded-full border-4 border-neutral-800 p-3 mx-1 xs:mx-2.5 sm:mx-10">
             <button>
               <img src="/cart.gif" alt="cart" className='size-10' />
             </button>
           </div></Link>
 
           <div className='flex justify-between items-center gap-4 xs:gap-8 sm:gap-15 mx-1 xs:mx-2.5'>
-            <Link href="/about" className={pathname === "/about" ? "bg-[#10b981] p-2.5 rounded-xl flex flex-col justify-center items-center text-white" : " flex flex-col justify-center items-center text-white"}>
+            <Link href="/about" className={pathname === "/about" ? "bg-neutral-200 p-2.5 rounded-xl flex flex-col justify-center items-center" : " flex flex-col justify-center items-center"}>
               <button>
-                <AiOutlineTeam className='text-white size-6 sm:size-8' />
+                <AiOutlineTeam className='size-6 sm:size-8' />
               </button>
               <div className='text-sm sm:text-base'>About</div>
             </Link>
-            <Link href="/contact" className={pathname === "/contact" ? "bg-[#10b981] p-2.5 rounded-xl flex flex-col justify-center items-center text-white" : " flex flex-col justify-center items-center text-white"}>
+            <Link href="/contact" className={pathname === "/contact" ? "bg-neutral-200 p-2.5 rounded-xl flex flex-col justify-center items-center" : " flex flex-col justify-center items-center"}>
               <button>
-                <RiContactsBook3Fill className='text-white size-6 sm:size-8' />
+                <RiContactsBook3Fill className='size-6 sm:size-8' />
               </button>
               <div className='text-sm sm:text-base'>Contact</div>
             </Link>
